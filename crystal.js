f4409e0eeab4948701f7c57dde99e9345afa72c2
Agsmith22 <31256060+Agsmith22@users.noreply.github.com>
2017-10-05 18:47:34 -0500

    Update crystal.js

--- crystal.js
+++ crystal.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
+  
+
+  $(document).ready(function(){
   var Random=Math.floor(Math.random()*101+19)
-  
   $("#randomNumber").text(Random);
- 
-    var num1= Math.floor(Math.random()*11+1)
-   var num2= Math.floor(Math.random()*11+1)     //GIVES A RANDOM NUMBER BETWEEN 1-12
-   var num3= Math.floor(Math.random()*11+1)
-    var num4= Math.floor(Math.random()*11+1)
-  
+    
+    var num1= Math.floor(Math.random()*11+1);
+   var num2= Math.floor(Math.random()*11+1);     //GIVES A RANDOM NUMBER BETWEEN 1-12
+   var num3= Math.floor(Math.random()*11+1);
+    var num4= Math.floor(Math.random()*11+1);
   var userTotal= 0; 
   var wins= 0;              /// WINS/LOSS AND GAME SCORE BOX
     var losses = 0;
